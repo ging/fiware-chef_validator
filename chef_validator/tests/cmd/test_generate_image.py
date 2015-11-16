b5004be268d70037619893c785a91650b33f509e
pmverdugo <pm.verdugo@alumnos.upm.es>
2015-11-16 14:34:01 +0100

    cmd dir test collision

--- chef_validator/tests/cmd/test_generate_image.py
+++ chef_validator/tests/cmd/test_generate_image.py
@@ -12,7 +12,7 @@
 #  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #  License for the specific language governing permissions and limitations
 #  under the License.
-""" Tests for chef_validator.cmd.generate_image """
+""" Tests for chef_validator.command.generate_image """
 from __future__ import unicode_literals
 
 import mock
--- tools/chef_cookbook/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['fiware-chef_validator'][:install_dir] = '/opt/fiware-chef_validator'
-default['fiware-chef_validator'][:host] = '0.0.0.0'
-default['fiware-chef_validator'][:port] = '4042'
-default['fiware-chef_validator'][:docker_url] = 'tcp://172.17.42.1:2375'
-default['fiware-chef_validator'][:keystone_url] = 'http://172.17.42.1:5000/v2.0'
-
