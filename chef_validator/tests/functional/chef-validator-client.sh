db7b2be140ebf0c83f5586862fc90b166f7966e2
pmverdugo <pm.verdugo@alumnos.upm.es>
2015-11-30 14:12:12 +0100

    tests wrappers update

--- chef_validator/tests/functional/chef-validator-client.sh
+++ chef_validator/tests/functional/chef-validator-client.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+# server
+python ../../command/chef-validator-api.py --config-dir=../../../etc/chef-validator
 # passed
 python chef-validator-client_test.py --username=demo --password=demo --validator_url=http://127.0.0.1:4042 --cookbook=git
 # error
