c98361186a75c7fcfd0d7caa88921c2e29b3910a
pmverdugo <pm.verdugo@alumnos.upm.es>
2015-11-30 12:56:38 +0100

    tox tests path update

--- chef_validator/tests/functional/chef-validator-client.sh
+++ chef_validator/tests/functional/chef-validator-client.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# passed
 python chef-validator-client_test.py --username=demo --password=demo --validator_url=http://127.0.0.1:4042 --cookbook=git
+# error
+python chef-validator-client_test.py --username=demo --password=demo --validator_url=http://127.0.0.1:4042 --cookbook=myfakecookbook
